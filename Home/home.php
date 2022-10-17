d1e157b7307788cb1583e6ef4d47566f0a377d8d
sosajua <jdsosas@correo.udistrital.edu.co}>
2022-10-17 11:34:47 -0500

    aplicacion 4 señales y calculo ruido

--- Home/home.php
+++ Home/home.php
@@ -7,7 +7,7 @@ include("../Senales/Lista_Senales.php");
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" language="javascript" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" language="javascript" href="../bootstrap-5.0.2-dist/css/bootstrap.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,7 +20,6 @@ include("../Senales/Lista_Senales.php");
     <div class="container">
         <h1>Graficas de Espectro</h1>
     </div>
-    <form action="" method="POST">
         <div class="col-lg-6">
             <h3 align="center">Señales</h3>
             <table class="table caption-top table-hover table-success">
@@ -41,9 +40,9 @@ include("../Senales/Lista_Senales.php");
                     for ($i = 0; $i < count($lists); $i++) {
                         echo "<tr>";
                         echo "<td>" . $lists[$i]['id'] . "</td>";
-                        echo "<td>" . $lists[$i]['Frecuencia Central'] . "</td>";
-                        echo "<td>" . $lists[$i]['Ancho de banda'] . "</td>";
-                        echo "<td>" . $lists[$i]['Potencia pico'] . "</td>";
+                        echo "<td>" . $lists[$i]['Frecuencia_Central'] . "</td>";
+                        echo "<td>" . $lists[$i]['Ancho_de_banda'] . "</td>";
+                        echo "<td>" . $lists[$i]['Potencia_pico'] . "</td>";
                         echo "<td>" . $lists[$i]['Temp'] . "</td>";
                     }
                     ?>
@@ -53,16 +52,307 @@ include("../Senales/Lista_Senales.php");
             </table>
             <tr>
         </div>
-    </form>
     <form action="../insertar/insertar.php" method="POST">
 
         <input type="number" name="frecuencia" id="frecuencia" placeholder="Frecuencia Central">
         <input type="number" name="BW" id="BW" placeholder="Acnho de Banda">
         <input type="number" name="PP" id="PP" placeholder="Potencia Pico">
-        <input type="number" name="Temp" id="Temp" placeholder="Temperatura">
+        <?php
+        if($lists!=null){
+            echo" <input disabled  value='".$lists[0]['Temp']."' '>";
+            echo" <input type='hidden' name='Temp' id='Temp' value='".$lists[0]['Temp']."' '>";
+        }else{
+            echo" <input type='number' name='Temp' id='Temp' placeholder='Temperatura'>";
+        }
+        ?>
         <input type="submit" name="subir" id="subir" value="Hecho">
     </form>
-    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <form action="./Home.php" method="POST">
+        <input type="submit" name="graficar" id="graficar" value="Graficar">
+    </form>
+    
+    <form action="./Home.php" method="POST">
+        <input type="submit" name="limpiar" id="limpiar" value="Limpiar">
+    </form>
+<?php
+
+if(isset($_POST['limpiar'])){
+
+}
+
+if(isset($_POST['graficar'])){
+    $obj = new CRUD_Senal();
+    $lista=$obj->SenalesORG();
+    if($lista == null ){
+    }else {
+        $t=$lista[count($lista)-1]['id'];
+        $bwm=$obj->SenalesBW();
+        $piso_ruido=log10(((pow(10,-23)*1.38)*(($bwm[0]['Ancho_de_banda'])/(10))*($bwm[0]['Temp']))/(pow(10,-3)*1))*10; 
+        //$piso_ruido=-100;
+        if ($t == 1) {            
+           $BW = $lista[0]['Ancho_de_banda']; 
+           $frecuencia=$lista[0]['Frecuencia_Central'];
+           $PP=$lista[0]['Potencia_pico']; 
+           echo "
+           <script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
+           <script type='text/javascript'>
+               google.charts.load('current', {
+                   'packages': ['line']
+               });
+               google.charts.setOnLoadCallback(drawChart);
+       
+               function drawChart() {
+                   var data = new google.visualization.DataTable();
+                   data.addColumn('number', 'Frecuencia (Mhz)');
+                   data.addColumn('number', 'señal');
+                   data.addColumn('number', 'Ruido');
+
+
+                   data.addRows([
+                    [".($frecuencia-($BW)).", ".$piso_ruido.",".$piso_ruido."],
+                    [".($frecuencia-($BW/2)).", ".($PP-3).",".$piso_ruido."],
+                    [".$frecuencia.", ".$PP.",".$piso_ruido."],
+                    [".($frecuencia+($BW/2)).", ".($PP-3).",".$piso_ruido."],
+                    [".($frecuencia+($BW)).", ".$piso_ruido.",".$piso_ruido."],
+                   ]);
+
+                   var options = {
+                       chart: {
+                           title: 'Visualizacion de señales de radiofrecuencia',
+                           subtitle: 'dBm'
+                       },
+                       width: 900,
+                       height: 500
+                   };
+       
+                   var chart = new google.charts.Line(document.getElementById('linechart_material'));
+       
+                   chart.draw(data, google.charts.Line.convertOptions(options));
+               }
+           </script>
+           <div id='linechart_material' style='width: 900px; height: 500px'></div>
+           ";
+        }
+        if ($t == 2) {            
+            $BW = $lista[0]['Ancho_de_banda']; 
+            $frecuencia=$lista[0]['Frecuencia_Central'];
+            $PP=$lista[0]['Potencia_pico'];
+
+            $BW1 = $lista[1]['Ancho_de_banda']; 
+            $frecuencia1=$lista[1]['Frecuencia_Central'];
+            $PP1=$lista[1]['Potencia_pico'];
+            
+            echo "
+            <script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
+            <script type='text/javascript'>
+                google.charts.load('current', {
+                    'packages': ['line']
+                });
+                google.charts.setOnLoadCallback(drawChart);
+        
+                function drawChart() {
+        
+                    var data = new google.visualization.DataTable();
+                    data.addColumn('number', 'Frecuencia (Mhz)');
+                    data.addColumn('number', 'señales');
+                    data.addColumn('number', 'ruido');
+ 
+                    data.addRows([
+                        [".($frecuencia-($BW)).", ".$piso_ruido.",".$piso_ruido."],
+                        [".($frecuencia-($BW/2)).", ".($PP-3).",".$piso_ruido."],
+                        [".$frecuencia.", ".$PP.",".$piso_ruido."],
+                        [".($frecuencia+($BW/2)).", ".($PP-3).",".$piso_ruido."],
+                        [".($frecuencia+($BW)).", ".$piso_ruido.",".$piso_ruido."],
+                       ]);
+
+                       data.addRows([
+                        [".($frecuencia1-($BW1)).", ".$piso_ruido.",".$piso_ruido."],
+                        [".($frecuencia1-($BW1/2)).", ".($PP1-3).",".$piso_ruido."],
+                        [".$frecuencia1.", ".$PP1.",".$piso_ruido."],
+                        [".($frecuencia1+($BW1/2)).", ".($PP1-3).",".$piso_ruido."],
+                        [".($frecuencia1+($BW1)).", ".$piso_ruido.",".$piso_ruido."],
+                       ]);
+
+                    var options = {
+                        chart: {
+                            title: 'Visualizacion de señales de radiofrecuencia',
+                            subtitle: 'dBm'
+                        },
+                        width: 900,
+                        height: 500
+                    };
+        
+                    var chart = new google.charts.Line(document.getElementById('linechart_material'));
+        
+                    chart.draw(data, google.charts.Line.convertOptions(options));
+                }
+            </script>
+            <div id='linechart_material' style='width: 900px; height: 500px'></div>
+            ";
+        }
+
+        if ($t == 3) {            
+
+            $BW = $lista[0]['Ancho_de_banda']; 
+            $frecuencia=$lista[0]['Frecuencia_Central'];
+            $PP=$lista[0]['Potencia_pico'];
+
+            $BW1 = $lista[1]['Ancho_de_banda']; 
+            $frecuencia1=$lista[1]['Frecuencia_Central'];
+            $PP1=$lista[1]['Potencia_pico'];
+
+            $BW2 = $lista[2]['Ancho_de_banda']; 
+            $frecuencia2=$lista[2]['Frecuencia_Central'];
+            $PP2=$lista[2]['Potencia_pico'];
+            
+            echo "
+            <script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
+            <script type='text/javascript'>
+                google.charts.load('current', {
+                    'packages': ['line']
+                });
+                google.charts.setOnLoadCallback(drawChart);
+        
+                function drawChart() {
+        
+                    var data = new google.visualization.DataTable();
+                    data.addColumn('number', 'Frecuencia (Mhz)');
+                    data.addColumn('number', 'señales');
+                    data.addColumn('number', 'ruido');
+ 
+                    data.addRows([
+                        [".($frecuencia-($BW)).", ".$piso_ruido.",".$piso_ruido."],
+                        [".($frecuencia-($BW/2)).", ".($PP-3).",".$piso_ruido."],
+                        [".$frecuencia.", ".$PP.",".$piso_ruido."],
+                        [".($frecuencia+($BW/2)).", ".($PP-3).",".$piso_ruido."],
+                        [".($frecuencia+($BW)).", ".$piso_ruido.",".$piso_ruido."],
+                       ]);
+
+                       data.addRows([
+                        [".($frecuencia1-($BW1)).", ".$piso_ruido.",".$piso_ruido."],
+                        [".($frecuencia1-($BW1/2)).", ".($PP1-3).",".$piso_ruido."],
+                        [".$frecuencia1.", ".$PP1.",".$piso_ruido."],
+                        [".($frecuencia1+($BW1/2)).", ".($PP1-3).",".$piso_ruido."],
+                        [".($frecuencia1+($BW1)).", ".$piso_ruido.",".$piso_ruido."],
+                       ]);
+
+                       data.addRows([
+                        [".($frecuencia2-($BW2)).", ".$piso_ruido.",".$piso_ruido."],
+                        [".($frecuencia2-($BW2/2)).", ".($PP2-3).",".$piso_ruido."],
+                        [".$frecuencia2.", ".$PP2.",".$piso_ruido."],
+                        [".($frecuencia2+($BW2/2)).", ".($PP2-3).",".$piso_ruido."],
+                        [".($frecuencia2+($BW2)).", ".$piso_ruido.",".$piso_ruido."],
+                       ]);
+
+                    var options = {
+                        chart: {
+                            title: 'Visualizacion de señales de radiofrecuencia',
+                            subtitle: 'dBm'
+                        },
+                        width: 900,
+                        height: 500
+                    };
+        
+                    var chart = new google.charts.Line(document.getElementById('linechart_material'));
+        
+                    chart.draw(data, google.charts.Line.convertOptions(options));
+                }
+            </script>
+            <div id='linechart_material' style='width: 900px; height: 500px'></div>
+            ";
+        }
+
+        if ($t == 4) {            
+            $BW = $lista[0]['Ancho_de_banda']; 
+            $frecuencia=$lista[0]['Frecuencia_Central'];
+            $PP=$lista[0]['Potencia_pico'];
+
+            $BW1 = $lista[1]['Ancho_de_banda']; 
+            $frecuencia1=$lista[1]['Frecuencia_Central'];
+            $PP1=$lista[1]['Potencia_pico'];
+
+            $BW2 = $lista[2]['Ancho_de_banda']; 
+            $frecuencia2=$lista[2]['Frecuencia_Central'];
+            $PP2=$lista[2]['Potencia_pico'];
+            
+            $BW3 = $lista[3]['Ancho_de_banda']; 
+            $frecuencia3=$lista[3]['Frecuencia_Central'];
+            $PP3=$lista[3]['Potencia_pico'];
+            
+            echo "
+            <script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
+            <script type='text/javascript'>
+                google.charts.load('current', {
+                    'packages': ['line']
+                });
+                google.charts.setOnLoadCallback(drawChart);
+        
+                function drawChart() {
+        
+                    var data = new google.visualization.DataTable();
+                    data.addColumn('number', 'Frecuencia (Mhz)');
+                    data.addColumn('number', 'señales');
+                    data.addColumn('number', 'ruido');
+ 
+                    data.addRows([
+                        [".($frecuencia-($BW)).", ".$piso_ruido.",".$piso_ruido."],
+                        [".($frecuencia-($BW/2)).", ".($PP-3).",".$piso_ruido."],
+                        [".$frecuencia.", ".$PP.",".$piso_ruido."],
+                        [".($frecuencia+($BW/2)).", ".($PP-3).",".$piso_ruido."],
+                        [".($frecuencia+($BW)).", ".$piso_ruido.",".$piso_ruido."],
+                       ]);
+
+                       data.addRows([
+                        [".($frecuencia1-($BW1)).", ".$piso_ruido.",".$piso_ruido."],
+                        [".($frecuencia1-($BW1/2)).", ".($PP1-3).",".$piso_ruido."],
+                        [".$frecuencia1.", ".$PP1.",".$piso_ruido."],
+                        [".($frecuencia1+($BW1/2)).", ".($PP1-3).",".$piso_ruido."],
+                        [".($frecuencia1+($BW1)).", ".$piso_ruido.",".$piso_ruido."],
+                       ]);
+
+                       data.addRows([
+                        [".($frecuencia2-($BW2)).", ".$piso_ruido.",".$piso_ruido."],
+                        [".($frecuencia2-($BW2/2)).", ".($PP2-3).",".$piso_ruido."],
+                        [".$frecuencia2.", ".$PP2.",".$piso_ruido."],
+                        [".($frecuencia2+($BW2/2)).", ".($PP2-3).",".$piso_ruido."],
+                        [".($frecuencia2+($BW2)).", ".$piso_ruido.",".$piso_ruido."],
+                       ]);
+
+                       data.addRows([
+                        [".($frecuencia3-($BW3)).", ".$piso_ruido.",".$piso_ruido."],
+                        [".($frecuencia3-($BW3/2)).", ".($PP3-3).",".$piso_ruido."],
+                        [".$frecuencia3.", ".$PP3.",".$piso_ruido."],
+                        [".($frecuencia3+($BW3/2)).", ".($PP3-3).",".$piso_ruido."],
+                        [".($frecuencia3+($BW3)).", ".$piso_ruido.",".$piso_ruido."],
+                       ]);
+
+                    var options = {
+                        chart: {
+                            title: 'Visualizacion de señales de radiofrecuencia',
+                            subtitle: 'dBm'
+                        },
+                        width: 900,
+                        height: 500
+                    };
+        
+                    var chart = new google.charts.Line(document.getElementById('linechart_material'));
+        
+                    chart.draw(data, google.charts.Line.convertOptions(options));
+                }
+            </script>
+            <div id='linechart_material' style='width: 900px; height: 500px'></div>
+            ";
+        }
+    }
+   
+    
+   
+}
+
+?>
+
+        
+    <script src="../bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- Senales/Lista_Senales.php
+++ Senales/Lista_Senales.php
@@ -7,7 +7,7 @@ class CRUD_Senal
     public function Insert($fc, $anchoBanda, $potenciaP, $temp)
     {
         if ($this->Validar4() == true) {
-            $sql = "INSERT INTO `senal`(`Frecuencia Central`,`Ancho de banda`, `Potencia pico`, `Temp`) VALUES ($fc,$anchoBanda,$potenciaP,$temp)";
+            $sql = "INSERT INTO `senal`(`Frecuencia_Central`,`Ancho_de_banda`, `Potencia_pico`, `Temp`) VALUES ($fc,$anchoBanda,$potenciaP,$temp)";
             $result = mysqli_query(Conection::conectar(), $sql) or die("Error en la consulta de insertar");
             echo "
              <script type='text/javascript'>
@@ -43,10 +43,39 @@ class CRUD_Senal
         while ($r = mysqli_fetch_array($result)) {
             $lista[] = $r;
         }
-        if ( $lista[count($lista)-1]['id'] == 4) {
-            return false;
-        } else {
+        if($lista!= null ){
+            if ( $lista[count($lista)-1]['id'] == 4) {
+                return false;
+            } else {
+                return true;
+            }
+        }else {
             return true;
         }
+        
+    }
+
+    public function SenalesORG(){
+        $sql = "SELECT * FROM Senal ORDER by 'Frecuencia_Central'";
+        $señal = array();
+        $result = mysqli_query(Conection::conectar(), $sql) or die("Error al consultar buscar id");
+        while ($r = mysqli_fetch_array($result)) {
+            $señal[] = $r;
+        }
+        return $señal;
+    }
+
+    public function SenalesBW(){
+        $sql = "SELECT * FROM Senal order by senal.Ancho_de_banda ASC;";
+        $señal = array();
+        $result = mysqli_query(Conection::conectar(), $sql) or die("Error al consultar buscar id");
+        while ($r = mysqli_fetch_array($result)) {
+            $señal[] = $r;
+        }
+        return $señal;
+    }
+
+    public function Limpiar(){
+        
     }
 }
